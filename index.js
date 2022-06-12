23dc66c2c45d9c9298429d848cce7f6ba09d7a5e
derrique <otieno.derrick@student.moringaschool.com>
2022-06-12 18:03:52 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const main = document.getElementById("main").remove();
+
+
+const victory = document.getElementById("victory");
+
+let newHeader = document.createElement('h1');
+
+document.body.appendChild(newHeader)
+
+ newHeader.id = "victory";
+
+newHeader.innerHTML = "Derrick is the champion";
